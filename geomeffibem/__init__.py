4b7b38fceeb6a726f7267faec6547e7d21ee82d7
Julien Marrec <julien.marrec@gmail.com>
2022-05-30 18:04:33 +0200

    Add classes and free functions to __init__.py for convenience

--- geomeffibem/__init__.py
+++ geomeffibem/__init__.py
@@ -3,3 +3,8 @@
 __author__ = """Julien Marrec"""
 __email__ = 'contact@effibem.com'
 __version__ = '0.1.0'
+
+from geomeffibem.plane import Plane
+from geomeffibem.polyhedron import Polyhedron
+from geomeffibem.surface import Surface, Surface3dEge, plot_vertices
+from geomeffibem.vertex import Vertex, distance, distanceFromPointToLine, isAlmostEqual3dPt
