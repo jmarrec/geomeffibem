a64d537ece4c2e8b59b8262ac3b435eda3a12829
Julien Marrec <julien.marrec@gmail.com>
2022-05-30 23:01:22 +0200

    Bump version: 0.1.0 → 0.1.1

--- geomeffibem/__init__.py
+++ geomeffibem/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Julien Marrec"""
 __email__ = 'contact@effibem.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from geomeffibem.plane import Plane
 from geomeffibem.polyhedron import Polyhedron
