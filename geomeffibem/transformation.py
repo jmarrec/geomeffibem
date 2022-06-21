7e19abeb3e5c6dcaa9b2828899d69a66365a4c69
Julien Marrec <julien.marrec@gmail.com>
2022-06-21 17:14:53 +0200

    Start on the Transformation module

---
+++ geomeffibem/transformation.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import numpy as np
+
+from geomeffibem.plane import Plane
+from geomeffibem.surface import Surface
+from geomeffibem.vertex import Vertex
+
+
+class Transformation:
+    @staticmethod
+    def Rotation(axis: Vertex, radians: float, point: Optional[Vertex] = None) -> Transformation:
+
+        temp = axis.normalize()
+        normalVector = temp.to_numpy()
+        P = np.outer(normalVector, normalVector)
+
+        # Rodrigues' rotation formula / Rotation matrix from Euler axis/angle
+        # I*cos(radians) + I*(1-cos(radians))*axis*axis^T + Q*sin(radians)
+        # Q = [0, -axis[2], axis[1]; axis[2], 0, -axis[0]; -axis[1], axis[0], 0]
+        Q = np.zeros(shape=(3, 3))
+        Q[0, 1] = -normalVector[2]
+        Q[0, 2] = normalVector[1]
+        Q[1, 0] = normalVector[2]
+        Q[1, 2] = -normalVector[0]
+        Q[2, 0] = -normalVector[1]
+        Q[2, 1] = normalVector[0]
+
+        c = np.cos(radians)
+        identity_3 = np.identity(3)
+        R = identity_3 * c + (1.0 - c) * P + Q * np.sin(radians)
+        result = np.identity(4)
+        result[:-1, :-1] = R
+        if point is not None:
+            #  translate point to origin, rotate, and then translate back
+            # t_ori = Transformation.Translation(point)
+            # t_rot = Transformation(result)
+            # t_reverse = Transformation.Translation(-point)
+            # return (t_ori, t_rot, t_reverse)
+            return Transformation.Translation(point) * Transformation(result) * Transformation.Translation(-point)
+
+        return Transformation(result)
+
+    @staticmethod
+    def Translation(translation: Vertex) -> Transformation:
+        result = np.identity(4)
+        result[0, 3] = translation.x
+        result[1, 3] = translation.y
+        result[2, 3] = translation.z
+        return Transformation(result)
+
+    # @staticmethod
+    # def RotationAroundPoint(point: Vertex, axis: Vertex, radians) -> Transformation
+
+    def __init__(self, matrix: np.ndarray = None):
+        if matrix is None:
+            matrix = np.identity(4)
+        elif matrix.shape != (4, 4):
+            raise ValueError(f"Expected a matrix of dimension (4, 4), got {matrix.shape}")
+        self.matrix = matrix
+
+    def rotationMatrix(self) -> np.ndarray:
+        return self.matrix[:-1, :-1]
+
+    def translation(self) -> np.ndarray:
+        return Vertex(self.matrix[0, 3], self.matrix[1, 3], self.matrix[2, 3])
+
+    def __mul__(self, other):
+        if isinstance(other, Vertex):
+            temp = np.matmul(self.matrix, np.append(other.to_numpy(), 1.0))
+            return Vertex(temp[0], temp[1], temp[2])
+        elif isinstance(other, Transformation):
+            return Transformation(np.matmul(self.matrix, other.matrix))
+        elif isinstance(other, np.ndarray) or isinstance(other, list):
+            return np.array([self * v for v in other])
+        elif isinstance(other, Surface):
+            name = "Rotated unnamed"
+            if other.name is not None:
+                name = f"Rotated {name}"
+
+            return Surface(vertices=[self * v for v in other.vertices], name=name)
+        elif isinstance(other, Plane):
+            # translate a point on the plane, just project (0,0,0)
+            point = other.project(Vertex(0.0, 0.0, 0.0))
+
+            # Get a point at outward normal
+            outwardNormal = other.outwardNormal()
+            refPoint = point + outwardNormal
+
+            # Translate the two points and recompute the normal
+            newPoint = self * point
+            newRefPoint = self * refPoint
+            newNormal = newRefPoint - newPoint
+
+            d = (-newNormal).dot(newPoint)
+
+            p = Plane(newNormal.x, newNormal.y, newNormal.z, d)
+
+            return p
+        else:
+            raise ValueError(f"Not implemented for type {type(other)}")
+
+    def __repr__(self):
+        return f"Transformation:\n{self.matrix.__str__()}"
